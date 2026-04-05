f4cc65ddb5eb3895be54dd0fff5d8955d14ae035
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2026-04-06 00:00:00 +0500

    Initialize todo app with tailwind installation

---
+++ todo/app/(tabs)/_layout.tsx
@@ -0,0 +1,43 @@
+import { Tabs } from "expo-router"
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from "@/constants/theme"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+const TabLayout = () => {
+    const insets = useSafeAreaInsets()
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: Colors.background,
+                    height: 72,
+                    width: "90%",
+                    marginHorizontal: "auto",
+                    borderRadius: 999,
+                    bottom: Math.max(20, insets.bottom)
+
+                }
+            }}
+        >
+
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon(props) {
+                        return <Ionicons
+                            name="home"
+                            className="text-foreground size-12"
+                        />
+                    },
+
+                }}
+            />
+
+        </Tabs>
+    )
+}
+
+export default TabLayout
---
+++ todo/constants/theme.ts
@@ -0,0 +1,4 @@
+export const Colors = {
+    background: "#9810fa",
+    foreground: "#fffff"
+} as const
