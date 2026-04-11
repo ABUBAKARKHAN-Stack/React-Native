023b1e9f550e3ab8de9250aa3f7de060e91000c7
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2026-04-11 19:53:41 +0500

    feat: initialize frontend project with Expo, TypeScript, and essential scripts
    - Added package.json for managing dependencies and scripts.
    - Created reset-project.js script to reset project structure.
    - Added tsconfig.json for TypeScript configuration.
    - Initialized package-lock.json for dependency locking.

--- chat-app/backend/src/middlewares/global-error.middleware.ts
+++ chat-app/backend/src/middlewares/global-error.middleware.ts
@@ -1,40 +1,62 @@
-import { NextFunction, Request, Response } from "express";
+import { DrizzleError } from "drizzle-orm";
 import { ApiError } from "../utils/ApiError";
+import { NextFunction, Request, Response } from "express";
 import { isAPIError } from "better-auth/api";
 
 const globalErrorHandler = (
     err: Error,
     _req: Request,
     res: Response,
+    next:NextFunction
 ) => {
 
-    //* (CASE) Api Error Class Instance
-    if (
-        err instanceof ApiError
-    ) {
+    const isDev = process.env.NODE_ENV === "development";
+
+    //* ApiError (Custom)
+    if (err instanceof ApiError) {
         return res
             .status(err.status)
-            .json(new ApiError(
-                err.status,
-                err.message,
-                err.error
-            ))
+            .json(
+                new ApiError(
+                    err.status,
+                    err.message,
+                    isDev ? err.error : undefined
+                )
+            );
     }
 
-    //* (CASE) Better Auth
+    //* Better Auth
     if (isAPIError(err)) {
         return res
-            .status(+err.status)
-            .json(new ApiError(
-                +err.status,
-                err.message,
-            ))
+            .status(Number(err.status))
+            .json(
+                new ApiError(
+                    Number(err.status),
+                    `Auth Error: ${err.message}`
+                )
+            );
     }
 
-    //* (CASE) Internal Server
-    return res
-        .status(500)
-        .json(new ApiError(500, "Internal Server Error"))
-}
+    //* Drizzle (DB Errors)
+    if (err instanceof DrizzleError) {
+        return res
+            .status(400)
+            .json(
+                new ApiError(
+                    400,
+                    `Database Error: ${err.message}`
+                )
+            );
+    }
+
+    //* Internal Server Error
+    return res.status(500).json(
+        new ApiError(
+            500,
+            "Internal Server Error",
+            isDev ? err.stack : undefined
+        )
+    );
+};
 
 export default globalErrorHandler
--- chat-app/backend/src/routes/health-check.route.ts
+++ chat-app/backend/src/routes/health-check.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { ApiResponse } from "../utils/ApiResponse";
+import { ApiError } from "../utils/ApiError";
+import { db } from "../lib/db";
+import { user } from "../models/schema";
 
 const router = Router()
 
-router.get('/', (_req, res) => {
+router.get('/', async (_req, res) => {
+    const users = await db.select().from(user)
+   
     return res
         .status(200)
-        .json(new ApiResponse(200, "System is Healthy :)"))
+        .json(new ApiResponse(200, "System is Healthy :)", users))
+
 })
 
 export default router
